a29c8c366a57ac7a9a7b34768aa6247be6c09d12
alanGarcia <220258@utags.edu.mx>
2025-11-04 12:41:22 -0600

    Feat: Config y PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,41 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy()
+    legacy(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["icon/logo.png"],
+
+      injectRegister: "auto",
+      srcDir: "public",
+      filename: "sw.js",
+      manifest: false,
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+  server: {
+    proxy: {
+      "/api/demonslayer": {
+        target: "https://www.demonslayer-api.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/demonslayer/, ""),
+        secure: false,
+      },
+    },
+  },
+});
