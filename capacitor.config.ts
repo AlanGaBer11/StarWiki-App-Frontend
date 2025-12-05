3e1b31c2835c93ffa6007eb6287f086661a3e928
alanGarcia <220258@utags.edu.mx>
2025-12-05 00:12:02 -0600

    Feat: Implementacion de splashscreen para app de android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,30 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
+  appId: "io.ionic.starter",
+  appName: "frontend",
+  webDir: "dist",
   plugins: {
     CapacitorCookies: {
       enabled: true,
     },
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: false,
+      // launchFadeOutDuration: 3000,
+      backgroundColor: "#414248",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: false,
+      splashImmersive: false,
+      layoutName: "launch_screen",
+      useDialog: false,
+    },
   },
-  appId: "io.ionic.starter",
-  appName: "frontend",
-  webDir: "dist",
 };
 
 export default config;
