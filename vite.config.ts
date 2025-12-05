f7cd43a122a0c19ae12eb70c570987801a056a9a
alanGarcia <220258@utags.edu.mx>
2025-12-05 05:20:40 -0600

    Fix, vitest y server

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -16,9 +18,25 @@ export default defineConfig({
         swSrc: "public/sw.js",
         swDest: "dist/sw.js",
         injectionPoint: undefined,
-        globPatterns: ["**/*.{js,css,html,ico,png,webp,jpg, jpeg, gif,svg}"],
+        globPatterns: ["**/*.{js,css,html,ico,png,webp,jpg,jpeg,gif,svg}"],
       },
       manifest: false,
     }),
   ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "/src/setupTests.ts",
+  },
+
+  server: {
+    proxy: {
+      "/api/demonslayer": {
+        target: "https://www.demonslayer-api.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/demonslayer/, ""),
+        secure: false,
+      },
+    },
+  },
 });
