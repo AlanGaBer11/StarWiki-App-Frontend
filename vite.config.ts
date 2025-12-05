5af549469552478e3729a441109c2bcb71c4acf6
alanGarcia <220258@utags.edu.mx>
2025-12-05 02:22:03 -0600

    Remove service worker and related caching logic; update Vite configuration for PWA integration

--- vite.config.ts
+++ vite.config.ts
@@ -5,31 +5,28 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     legacy(),
     VitePWA({
       registerType: "autoUpdate",
-
-      // NO USAR WORKBOX, NO USAR INJECTMANIFEST
-      strategies: "generateSW",
-      srcDir: "public/pwa",
-      filename: "service-worker.js",
-      manifest: false,
+      includeAssets: ["icon/logo.png"],
+      manifest: false, // usa tu manifest existente
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,webp,jpg,svg}"],
+      },
       devOptions: {
-        enabled: true,
-        type: "module",
+        enabled: true, // para ver sw.js en modo dev
       },
     }),
   ],
-
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
   },
-
   server: {
     proxy: {
       "/api/demonslayer": {
